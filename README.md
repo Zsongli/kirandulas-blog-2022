578a4e55b43b5abc03d9f775933bc829ba19e18d
Zsong Lee <zsongli@gmail.com>
2022-09-13 20:19:13 +0200

    Initial commit, project set up

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tanulmányi Kirándulás Blog (2022)
+> Iskolai projektmunka
+Újra SvelteKit-ben írva.
+[https://kirandulas-2022.zsong.li]
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+    <h1 class="font-bold">Welcome to SvelteKit</h1>
+    <p>Visit <a rel="external" target="blank" href="https://kit.svelte.dev" class="text-blue-700 underline">kit.svelte.dev</a> to read the documentation</p>
+</div>
