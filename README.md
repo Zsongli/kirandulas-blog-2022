3689d65ded3af81bbc70e2cd7ed7c5c638a8bf2b
Zsong Lee <zsongli@gmail.com>
2022-09-19 19:26:29 +0200

    add fontawesome, footer, and update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Tanulmányi Kirándulás Blog (2022)
-> Iskolai projektmunka
+> Iskolai projektmunka  
 Újra SvelteKit-ben írva.
 [https://kirandulas-2022.zsong.li]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
-<div class="hero absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-base-200">
-    <div class="hero-content text-center"> 
-        <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Üdvözlet!</h1>
-            <p class="py-6">Az oldal éppen készülőben van. Látogass vissza kicsit később, vagy figyeld a kód változtatásait Github-on!</p>
-            <a rel="external" href="https://github.com/Zsongli/kirandulas-blog-2022" class="btn btn-primary">Github</a>
-        </div>
-    </div>
+<script lang="ts">
+	import { faGithub } from "@fortawesome/free-brands-svg-icons";
+	import Fa from "svelte-fa";
+</script>
+
+<div class="hero bg-neutral mt-[20vh]">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Üdvözlet!</h1>
+			<p class="py-6">
+				Az oldal éppen készülőben van. Látogass vissza kicsit később, vagy fedezd fel a kódot
+				változás közben Github-on!
+			</p>
+			<a
+				rel="external"
+				href="https://github.com/Zsongli/kirandulas-blog-2022"
+				class="btn btn-primary gap-2"><Fa icon={faGithub} />Github</a
+			>
+		</div>
+	</div>
 </div>
