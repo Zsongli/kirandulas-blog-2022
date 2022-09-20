5d169653bc775daad16e2b530f8fd2d439641a93
Zsong Lee <zsongli@gmail.com>
2022-09-20 22:03:33 +0200

    move footer and custom icons into separate files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { faGithub } from "@fortawesome/free-brands-svg-icons";
 	import Fa from "svelte-fa";
+	import { faGithub } from "@fortawesome/free-brands-svg-icons";
 </script>
 
 <div class="hero bg-neutral mt-[20vh]">
