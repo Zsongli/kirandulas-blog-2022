55bd874837b1821fb8e187025acb7733af9f579c
Zsong Lee <zsongli@gmail.com>
2022-10-09 04:35:58 +0200

    add a navbar, custom scrollbar and update footer

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import Fa from "svelte-fa";
+	import { menuIcon } from "$lib/customIcons";
+	import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
+	export var title: string = "Title";
+	export var icon: IconDefinition | undefined = undefined;
+</script>
+
+<nav class="navbar grid grid-flow-col bg-base-100 rounded-2xl shadow-md">
+	<div class="gap-2 place-self-start">
+		<div class="dropdown h-full lg:hidden">
+			<div tabindex="0" class="btn btn-ghost h-full">
+				<Fa icon={menuIcon} />
+			</div>
+
+			<div class="dropdown-content w-max bg-base-100 p-3 rounded-box shadow-lg border-base-content border border-opacity-25">
+				<slot name="center" />
+			</div>
+		</div>
+
+		<a class="hidden lg:flex btn btn-ghost normal-case text-xl gap-3" href="/">
+			<Fa {icon} /> {title}</a
+		>
+
+		<slot name="start" />
+	</div>
+
+	<a class="lg:hidden flex btn btn-ghost normal-case text-2xl gap-3" href="/">
+		<Fa {icon} size="1.2x" /> {title}</a
+	>
+
+	<div class="hidden lg:flex place-self-center">
+		<slot name="center" />
+	</div>
+
+	<div class="place-self-end">
+		<slot name="end" />
+	</div>
+</nav>
+
+<style lang="postcss">
+	nav {
+		grid-template-columns: 1fr 2fr 1fr;
+	}
+
+	nav :global(ul.menu) > :global(li) > :global(a) {
+		border-radius: var(--rounded-btn);
+	}
+
+	nav .dropdown-content :global(ul) {
+		@apply flex-col;
+	}
+
+	nav .dropdown-content :global(ul) > :global(li) > :global(a) {
+		@apply w-full justify-center;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
 	import "$lib/styles/app.pcss";
 
+	import Nav from "$lib/components/Nav.svelte";
+
 	import PageLoader from "$lib/components/PageLoader.svelte";
 	import { navigating } from "$app/stores";
-	
+
 	import Footer from "$lib/components/Footer.svelte";
 	import Fa from "svelte-fa";
 	import { faGithub } from "@fortawesome/free-brands-svg-icons";
 	import { svelteIcon, szlgIcon } from "$lib/customIcons";
 	import { faGlobeEurope } from "@fortawesome/free-solid-svg-icons";
 </script>
+
+<Nav title="Bruh" icon={faGlobeEurope}>
+	<svelte:fragment slot="center">
+		<ul class="menu menu-horizontal gap-1">
+			<li><a href="nap1">Első nap</a></li>
+			<li><a href="nap2">Második nap</a></li>
+			<li><a href="nap3">Harmadik nap</a></li>
+		</ul>
+	</svelte:fragment>
+</Nav>
 
 {#if $navigating != null}
 	<PageLoader message="Egy pillanat..." />
@@ -20,19 +32,21 @@
 </main>
 
 <Footer>
-	<svelte:fragment slot="left">
+	<svelte:fragment slot="start">
 		<a href="/" class="link">
 			<Fa icon={faGlobeEurope} size="3x" />
 		</a>
 		<div class="flex flex-col">
-			<span class="font-semibold uppercase text-xs md:text-sm">2022 - SZENT LÁSZLÓ GIMNÁZIUM, BUDAPEST</span>
+			<span class="font-semibold uppercase"
+				>2022 - SZENT LÁSZLÓ GIMNÁZIUM, BUDAPEST</span
+			>
 			<span class="opacity-25 italic text-xs"
 				>A weboldalt készítette Makai Tamás és Mélykuti Ádám</span
 			>
 		</div>
 	</svelte:fragment>
-	
-	<svelte:fragment slot="right">
+
+	<svelte:fragment slot="end">
 		<a rel="external" target="_blank" href="http://szlgbp.hu" class="link">
 			<Fa icon={szlgIcon} size="2x" />
 		</a>
