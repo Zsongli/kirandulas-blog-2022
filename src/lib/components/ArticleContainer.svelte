5a99319b51610213ce5748420f54eabdd92105d9
Toma <tomasajtofficial@gmail.com>
2022-10-09 22:56:22 +0200

    add day3, readd intelligent padding

--- src/lib/components/ArticleContainer.svelte
+++ src/lib/components/ArticleContainer.svelte
@@ -8,39 +8,41 @@
 	export var nextHref: string | undefined = undefined;
 </script>
 
-<div class="max-w-5xl mx-auto bg-base-200 flex flex-col gap-8 px-16 pt-4 pb-16">
-	<div class="absolute top-0 invisible" id="top" />
+<div class="max-w-5xl mx-auto bg-base-200 pt-4 pb-16">
+	<div class="max-w-4xl mx-auto flex flex-col gap-8 px-4">
+		<div class="absolute top-0 invisible" id="top" />
 
-	<section class="leading-7">
-		<div class="flex flex-col w-11/12 m-auto justify-around">
-			<h1 class="uppercase font-bold text-4xl md:text-5xl text-center p-8">{title}</h1>
-			<div class="description-container">
-				<slot name="description" />
+		<section class="leading-7">
+			<div class="flex flex-col w-11/12 m-auto justify-around">
+				<h1 class="uppercase font-bold text-4xl md:text-5xl text-center p-8">{title}</h1>
+				<div class="description-container">
+					<slot name="description" />
+				</div>
 			</div>
+			<div class="divider">{date}</div>
+			<slot />
+		</section>
+	
+		<div class="divider" />
+	
+		<div class="flex items-center justify-around gap-4 flex-wrap">
+			{#if prevHref}
+				<a href={prevHref} class="btn btn-primary gap-4 w-40">
+					<Fa icon={faArrowLeft} size="1.5x" />
+					Előző
+				</a>
+			{/if}
+			<a href="#top" class="btn btn-secondary gap-4 w-48">
+				Vissza az elejére
+				<Fa icon={faArrowTurnUp} size="1.5x" />
+			</a>
+			{#if nextHref}
+				<a href={nextHref} class="btn btn-primary gap-4 w-40">
+					Következő
+					<Fa icon={faArrowRight} size="1.5x" />
+				</a>
+			{/if}
 		</div>
-		<div class="divider">{date}</div>
-		<slot />
-	</section>
-
-	<div class="divider" />
-
-	<div class="flex items-center justify-around gap-4 flex-wrap">
-		{#if prevHref}
-			<a href={prevHref} class="btn btn-primary gap-4 w-40">
-				<Fa icon={faArrowLeft} size="1.5x" />
-				Előző
-			</a>
-		{/if}
-		<a href="#top" class="btn btn-secondary gap-4 w-48">
-			Vissza az elejére
-			<Fa icon={faArrowTurnUp} size="1.5x" />
-		</a>
-		{#if nextHref}
-			<a href={nextHref} class="btn btn-primary gap-4 w-40">
-				Következő
-				<Fa icon={faArrowRight} size="1.5x" />
-			</a>
-		{/if}
 	</div>
 </div>
 
---
+++ src/routes/nap3/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	import CustomImage from "$lib/components/CustomImage.svelte";
+	import DayTemplate from "$lib/components/DayTemplate.svelte";
+	let audio1: HTMLAudioElement;
+	let audio2: HTMLAudioElement;
+	let dead = false;
+	let playing = false;
+	function funny() {
+		if (playing) return;
+		playing = true;
+		dead = !dead;
+		(dead ? audio1 : audio2).play();
+	}
+</script>
+
+<svelte:head>
+	<title>Kirándulás - 1. Nap</title>
+</svelte:head>
+
+<DayTemplate>
+	<h1 class="text-2xl">A harmadik nap</h1>
+	<article>
+		<h2 class="text-xl mb-2">Kőszeg - Jurisics-vár</h2>
+		<p>
+			A harmadik és egyben utolsó napon gyors osszepakolás után útnak indultunk a nap első
+			megállójához Kőszeghez. Itt a Jurisics-várat látogattuk meg, és még tervben volt egy szabad
+			séta is, de végül azt inkább a következő helyre hagytuk. A vár kapuján belépve elénk tárult az
+			udvar, ami most le volt zárva mert egy színpad fel volt állítva(?). Volt itt egy [szuvenír]
+			bolt is, sokan egyből elkezdtek kardozni a kirakott játékfegyverekkel. Ezután bemehettünk a
+			valódi kiállítás részre, de még mielőtt elengedtek volna, adtak egy feladatsort, amit a
+			látottak alapján majd ki tudnánk tölteni. Én (Toma), Ádám, Domi és Zsombor együtt töltöttük a
+			feladatsort ki, legalábbis ez volt a terv mert Ádámot az elején elveszítettük :/
+		</p>
+		<p>
+			Sajnos már nincs meg a feladatsor, de néhány feladatra emlékszem. Volt egy olyan, amihez fel
+			kellett mászi a harang tornyába. Több olyan feladat volt, ahol a plakátokon kiírt
+			információkat kellett leolvasni, vagy meg kellett számolni dolgokat. A legnehezebb feladat egy
+			falba vésett betű megtalálása volt. Több csapat kereste egyszerre, mégis csak sok idő után
+			lett meg. Ez a feladatsor valójában egy verseny volt, hogy ki tud több kérdést helyesen
+			megválaszolni. Nekünk sikerült is 1 hibával, amivel elsők lettünk. (50%-os matek ötös (bruh))
+		</p>
+		<div class="mt-6 mb-4 grid grid-cols-2 gap-4">
+			<CustomImage src="/img/jurisics-outer.jpg" desc="A vár kívülről" />
+			<CustomImage src="/img/jurisics-garden.jpg" desc="A belső kert ketreccel" />
+		</div>
+		<div class="mb-6 mt-4 grid grid-cols-4 gap-4">
+			<CustomImage src="/img/jurisics-miklos.jpg" desc="Jurisics Miklós szobra" />
+			<CustomImage src="/img/jurisics-bell.jpg" desc="Ébresztő mindenkinek" />
+			<CustomImage src="/img/jurisics-1v1.jpg" desc="Intenzív leszámolás" />
+			<CustomImage src="/img/jurisics-shoe.jpg" desc="Győzedelmi cipő felhúzva" />
+		</div>
+	</article>
+	<article>
+		<h2 class="text-xl mb-2">Szombathely - Iseum</h2>
+		<p>
+			Utolsó előtti megállónk Szombathely volt. Sajnos csak péntek volt, ezért nem volt teljes a
+			kirándulás :/ Először elmentünk megnézni az Iseumot, hosszabb nevén az Ízisz-múzeumot, ami a
+			restaurált Ízisz-szentélyben és a környező épületekben állított ki dolgokat. Először magába a
+			szentélybe mentünk be, majd visszamentünk a főépületbe, ahol a tárlatvezetőnk elmesélte a hely
+			törénetét.
+		</p>
+		<p>
+			A múzeumlátogatás után elengedtek minket enni valamit. Először a McDonald's-ba mentünk volna,
+			de sajnos olyan hosszú volt a sor, hogy abba beleöregedtünk volna. Ezért inkább a kevesebb,
+			mint száz méterre levő pizzázóba mentünk enni. Viszonylag gyorsan kész lett, így még maradt
+			egy kis időnk körbenézni a környéken.
+		</p>
+		<div class="mt-6 mb-4 grid grid-cols-4 gap-4">
+			<CustomImage src="/img/szombat-is-front.jpg" desc="Az Iseum" />
+			<CustomImage src="/img/szombat-is-isis.jpg" desc="Ízisz isteennő szobra" />
+			<CustomImage src="/img/szombat-is-explain.jpg" desc="A tárlatvezető magyaráz" />
+			<CustomImage src="/img/szombat-eat.jpg" desc="Éhes gyermek pizzát eszik" />
+		</div>
+		<div class="mb-6 mt-4 grid grid-cols-2 gap-4">
+			<div class="relative">
+				<CustomImage src="/img/szombat-is-dry.jpg" desc="💀💀" />
+				<div
+					class="absolute bottom-[0%] left-[15%] right-[15%] top-[70%]"
+					on:click={funny}
+					title="Ne kattints rám"
+				/>
+			</div>
+			<CustomImage src="/img/szombat-is-pieces.jpg" desc="Márványdarabok a lerombolt szentélyből" />
+		</div>
+		<audio src="/audio/dry1.wav" bind:this={audio1} on:ended={() => (playing = false)} />
+		<audio src="/audio/dry2.wav" bind:this={audio2} on:ended={() => (playing = false)} />
+	</article>
+</DayTemplate>
