d5cf3367a5f188333c8a2a97cbdb172fc0569a54
Zsong Lee <zsongli@gmail.com>
2022-10-17 18:00:07 +0200

    use postcss-nesting, because it's compatible with tailwind

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,7 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const presetEnv = require("postcss-preset-env");
-
 module.exports = {
-	plugins: [
-		tailwindcss(),
-		autoprefixer,
-		presetEnv({ features: { "nesting-rules": true } }),
-	]
+	plugins: {
+		"tailwindcss/nesting" : "postcss-nesting",
+		"tailwindcss" : {},
+		"autoprefixer" : {},
+	}
 };
